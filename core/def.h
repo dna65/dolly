42f02f8c6f54dc54ca33b40e726cd80539cf0411
dna <dna@thoroughbase.com>
2026-03-27 10:17:04 +0000

    Mark abort_no_mem() as cold path function

--- core/def.h
+++ core/def.h
@@ -1,3 +1,9 @@
 #pragma once
 
 typedef unsigned char uchar_t;
+
+#if defined(__GNUC__) || defined(__clang__)
+#define __COLD__ __attribute__((cold))
+#else
+#define __COLD__
+#endif
--- core/memory.h
+++ core/memory.h
@@ -2,7 +2,9 @@
 
 #include <stddef.h>
 
-void abort_no_mem(void);
+#include "core/def.h"
+
+void abort_no_mem(void) __COLD__;
 
 void* malloc_or_abort(size_t size);
 void* realloc_or_abort(void* old_pointer, size_t size);
